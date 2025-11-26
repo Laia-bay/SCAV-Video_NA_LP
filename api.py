3f79efbe6a9a8b833e005058f3b80f145c8db200
LaiaPertusa <92031213+Laia-bay@users.noreply.github.com>
2025-11-26 18:40:26 +0100

    seminar02 started
    he intentat fer el endpoint per la resize del video, pero triga massa, potser el podem tallar (que son 10 minuts de video)
    si no, hauriem de modificar el waiting time i no se com es fa

--- api.py
+++ api.py
@@ -4,13 +4,16 @@ from PIL import Image
 import numpy as np
 import os
 from first_seminar import colors, resizing, serpentine_pattern, black_white, RLE, DCT, DWT
+import ffmpeg
 
 app = FastAPI()
 
 IMAGE_FOLDER = "images"
 RESULT_FOLDER = "result_images"
 
-os.makedirs(RESULT_FOLDER, exist_ok=True)
+os.makedirs(RESULT_FOLDER, exist_ok=True)   ## to make sure that the folder where the modified images will be saved exists.
+
+
 @app.post("/serpentine")
 async def serpentine_endpoint(file: UploadFile = File(...)):
     img_bytes = await file.read()
@@ -41,3 +44,19 @@ async def bw_endpoint(file: UploadFile = File(...)):
 
     with open(output_path, "rb") as f:
         return Response(content=f.read(), media_type="image/jpg")
+    
+
+@app.post("/resize_video")
+async def resize_video_endpoint(file: UploadFile = File(...)):
+    img_bytes = await file.read()
+
+    input_path = "temp_input.mp4"
+    output_path = "image_results/resized_video.mp4"
+
+    with open(input_path, "wb") as f:
+        f.write(img_bytes)
+    
+    ffmpeg.input(input_path).output(output_path, vf=f"scale={1280/2}:{720/2}").run(capture_stdout=True, capture_stderr=True, overwrite_output=True)
+
+    with open(output_path, "rb") as f:
+        return Response(content=f.read(), media_type="video/mp4")
