80ede2d827d491f5998e41adbcfa8c0f77a273d7
LaiaPertusa <92031213+Laia-bay@users.noreply.github.com>
2025-12-03 00:29:29 +0100

    modified api version with website_laia changes
    a la meva branch metre feia la app he modificat coses del api.py, les modifico aquí per a que tinguem el mateix file.
    en resum es: poder escollir la transformació del bbb container
    arreglar el yuv histogram (retornava un "video" en negre)
    i poder escollir el convert dels codecs.

--- api.py
+++ api.py
@@ -124,18 +124,14 @@ async def video_info_endpoint(file: UploadFile = File(...)):
     return JSONResponse(content=five_data)
 
 @app.post("/create_BBB_container")
-async def create_BBB_container_endpoint(file: UploadFile = File(...), 
-                                        AAC_audio: bool = Form(...), MP3_audio: bool = Form(...), AC3_audio: bool = Form(...)):
+async def create_BBB_container_endpoint(file: UploadFile = File(...), AAC_audio: bool = Form(...), MP3_audio: bool = Form(...), AC3_audio: bool = Form(...)):
     video_bytes = await file.read()
 
     input_path = "images/temp_bbb_input.mp4"
     trimmed_video_path = "images/temp_bbb_trimmed.mp4"
-    if AAC_audio == True:
-        aac_audio_path = "images/aac_mono_audio.aac"
-    if MP3_audio == True:
-        mp3_audio_path = "images/mp3_stereo_audio.mp3"
-    if AC3_audio == True:
-        ac3_audio_path = "images/ac3_audio.ac3"
+    aac_audio_path = "images/aac_mono_audio.aac"
+    mp3_audio_path = "images/mp3_stereo_audio.mp3"
+    ac3_audio_path = "images/ac3_audio.ac3"
     output_path = "image_results/test_BBB_container.mp4"
 
     with open(input_path, "wb") as f:
@@ -144,98 +140,35 @@ async def create_BBB_container_endpoint(file: UploadFile = File(...),
     # cut first 20 seconds of the video
     ffmpeg.input(input_path).output(trimmed_video_path,t=20,vcodec="libx264").run(overwrite_output=True, capture_stdout=True, capture_stderr=True)
 
-    # create AAC mono audio
-    if AAC_audio == True:
-        ffmpeg.input(trimmed_video_path).output(aac_audio_path,acodec="aac",ac=1).run(overwrite_output=True, capture_stdout=True, capture_stderr=True)
-        aac_input = ffmpeg.input(aac_audio_path)
+    audio_tracks = []
+
+    if AAC_audio:
+        ffmpeg.input(trimmed_video_path).output(aac_audio_path, acodec="aac", ac=1).run(overwrite_output=True)
+        audio_tracks.append(aac_audio_path)
+
+    if MP3_audio:
+        ffmpeg.input(trimmed_video_path).output(mp3_audio_path, acodec="mp3", ac=2).run(overwrite_output=True)
+        audio_tracks.append(mp3_audio_path)
     
-    # create MP3 stereo audio with lower bitrate
-    # initial bitrate = 208216 bps (we obtained this value using the previous endpoint that showed the data of the video)
-    if MP3_audio == True:
-        ffmpeg.input(trimmed_video_path).output(mp3_audio_path,acodec="mp3",ac=2,audio_bitrate="128k").run(capture_stdout=True, capture_stderr=True, overwrite_output=True)
-        mp3_input = ffmpeg.input(mp3_audio_path)
-   
-    # create AC3 mono audio
-    if AC3_audio == True:
-        ffmpeg.input(trimmed_video_path).output(ac3_audio_path,acodec="ac3",ac=2).run(capture_stdout=True, capture_stderr=True, overwrite_output=True)
-        ac3_input = ffmpeg.input(ac3_audio_path)
-        
-    # package everything into a single MP4 container
+    if AC3_audio:
+        ffmpeg.input(trimmed_video_path).output(ac3_audio_path, acodec="ac3", ac=2).run(overwrite_output=True)
+        audio_tracks.append(ac3_audio_path)
+
+    if not audio_tracks:
+        return JSONResponse({"ERROR": "Select at least ONE audio track"}, status_code=400)
+
     video_input = ffmpeg.input(trimmed_video_path)
-    if AAC_audio == True and MP3_audio == True and AC3_audio == True:
-        ffmpeg.output(
-            video_input, aac_input, mp3_input, ac3_input,
-            output_path,
-            vcodec="copy",
-            acodec="copy",
-        ).run(capture_stdout=True, capture_stderr=True, overwrite_output=True)
-        os.remove(aac_audio_path)
-        os.remove(mp3_audio_path)
-        os.remove(ac3_audio_path)
+    input = [video_input] + [ffmpeg.input(a) for a in audio_tracks]
     
-    if AAC_audio == True and MP3_audio == True and AC3_audio == False:
-        ffmpeg.output(
-            video_input, aac_input, mp3_input,
-            output_path,
-            vcodec="copy",
-            acodec="copy",
-        ).run(capture_stdout=True, capture_stderr=True, overwrite_output=True)
-        os.remove(aac_audio_path)
-        os.remove(mp3_audio_path)
+    ffmpeg.output(video_input, *input, output_path, vcodec="copy", acodec="copy", strict="experimental").run(overwrite_output=True)
 
-    if AAC_audio == True and MP3_audio == False and AC3_audio == False:
-        ffmpeg.output(
-            video_input, aac_input,
-            output_path,
-            vcodec="copy",
-            acodec="copy",
-        ).run(capture_stdout=True, capture_stderr=True, overwrite_output=True)
-        os.remove(aac_audio_path)
-
-    if AAC_audio == True and MP3_audio == False and AC3_audio == True:
-        ffmpeg.output(
-            video_input, aac_input, ac3_input,
-            output_path,
-            vcodec="copy",
-            acodec="copy",
-        ).run(capture_stdout=True, capture_stderr=True, overwrite_output=True)
-        os.remove(aac_audio_path)
-        os.remove(ac3_audio_path)
-
-    if AAC_audio == False and MP3_audio == True and AC3_audio == True:
-        ffmpeg.output(
-            video_input, mp3_input, ac3_input,
-            output_path,
-            vcodec="copy",
-            acodec="copy",
-        ).run(capture_stdout=True, capture_stderr=True, overwrite_output=True)
-        os.remove(mp3_audio_path)
-        os.remove(ac3_audio_path)
-
-    if AAC_audio == False and MP3_audio == False and AC3_audio == True:
-        ffmpeg.output(
-            video_input, ac3_input,
-            output_path,
-            vcodec="copy",
-            acodec="copy",
-        ).run(capture_stdout=True, capture_stderr=True, overwrite_output=True)
-        os.remove(ac3_audio_path)
-
-    if AAC_audio == False and MP3_audio == True and AC3_audio == False:
-        ffmpeg.output(
-            video_input, mp3_input,
-            output_path,
-            vcodec="copy",
-            acodec="copy",
-        ).run(capture_stdout=True, capture_stderr=True, overwrite_output=True)
-        os.remove(mp3_audio_path)
-
-    os.remove(input_path)
-    os.remove(trimmed_video_path)
-    
-
+    for path in [aac_audio_path, mp3_audio_path, ac3_audio_path, input_path, trimmed_video_path]:
+        if os.path.exists(path):
+            os.remove(path)
+   
     with open(output_path, "rb") as f:
         return Response(content=f.read(), media_type="video/mp4")
+
     
 # Endpoint to inspect mp4 tracks 
 @app.post("/inspect_mp4_tracks")
@@ -285,7 +218,7 @@ async def yuv_histogram_endpoint(file: UploadFile = File(...)):
     with open(input_path, "wb") as f:
         f.write(video_bytes)
 
-    ffmpeg.input(input_path).output(output_path,vf="histogram").run(capture_stdout=True, capture_stderr=True, overwrite_output=True)
+    ffmpeg.input(input_path).output(output_path,vf="histogram", vcodec="libx264",pix_fmt="yuv420p").run(capture_stdout=True, capture_stderr=True, overwrite_output=True)
     
     os.remove(input_path)
     
@@ -294,31 +227,59 @@ async def yuv_histogram_endpoint(file: UploadFile = File(...)):
     
 # Endpoint to convert any input video into VP8, VP9, h265 & AV1
 @app.post("/convert_video_format")
-async def convert_video_format_endpoint(file: UploadFile = File(...)):
+async def convert_video_format_endpoint(file: UploadFile = File(...), VP8: bool = Form(...), VP9: bool = Form(...), h265: bool = Form(...)):
     video_bytes = await file.read()
 
     input_path = "images/temp_input.mp4"
-    #output_vp8_path = "image_results/test_convert_vp8.webm"
-    #output_vp9_path = "image_results/test_convert_vp9.webm"
+    output_vp8_path = "image_results/test_convert_vp8.webm"
+    output_vp9_path = "image_results/test_convert_vp9.webm"
     output_h265_path = "image_results/test_convert_h265.mp4"
 
     with open(input_path, "wb") as f:   
         f.write(video_bytes)
 
+    conversions = []
     # Convert video to VP8
-    #stream = ffmpeg.input(input_path)
-    #stream = ffmpeg.output(stream,output_vp8_path,vcodec="vp8",acodec="libvorbis",**{"q:v": 20})
-    #ffmpeg.run(stream,capture_stdout=True, capture_stderr=True, overwrite_output=True)
+    if VP8 == True:
+        stream = ffmpeg.input(input_path)
+        stream = ffmpeg.output(stream,output_vp8_path,vcodec="vp8",acodec="libvorbis",**{"q:v": 20})
+        ffmpeg.run(stream,capture_stdout=True, capture_stderr=True, overwrite_output=True)
+        conversions.append(output_vp8_path)
+        
 
     # Convert video to VP9
-    #stream = ffmpeg.input(input_path)
-    #stream = ffmpeg.output(stream,output_vp9_path,vcodec="vp9",acodec="libvorbis",**{"q:v": 20})
-    #ffmpeg.run(stream,capture_stdout=True, capture_stderr=True, overwrite_output=True)
-
+    if VP9 == True:
+        stream = ffmpeg.input(input_path)
+        stream = ffmpeg.output(stream,output_vp9_path,vcodec="vp9",acodec="libvorbis",**{"q:v": 20})
+        ffmpeg.run(stream,capture_stdout=True, capture_stderr=True, overwrite_output=True)
+        conversions.append(output_vp9_path)
+    
     # Convert video to h265
-    stream = ffmpeg.input(input_path)
-    stream = ffmpeg.output(stream,output_h265_path,vcodec="libx265",acodec="aac",**{"q:v": 20})
-    ffmpeg.run(stream,capture_stdout=True, capture_stderr=True, overwrite_output=True)
+    if h265 == True:
+        stream = ffmpeg.input(input_path)
+        stream = ffmpeg.output(stream,output_h265_path,vcodec="libx265",acodec="aac",**{"q:v": 20})
+        ffmpeg.run(stream,capture_stdout=True, capture_stderr=True, overwrite_output=True)
+        conversions.append(output_h265_path)
+    
+    if not conversions:
+        return JSONResponse({"ERROR": "Select at least ONE video format to convert"}, status_code=400)
+    
+    if len(conversions) == 1:
+        output_file = conversions[0]
+        with open(output_file, "rb") as f:
+            file_bytes = f.read()
 
-    with open(output_h265_path, "rb") as f:
-        return Response(content=f.read(), media_type="video/mp4") 
+        if output_file.endswith(".webm"):
+            media_type = "video/webm" 
+        else:
+            media_type = "video/mp4"
+        
+        return Response(content=file_bytes, media_type=media_type)
+    
+    response = {"converted_files": conversions.copy()}
+    
+    for path in [output_h265_path, output_vp9_path, output_vp8_path, input_path]:
+        if os.path.exists(path):
+            os.remove(path)
+    
+    return JSONResponse(response)
