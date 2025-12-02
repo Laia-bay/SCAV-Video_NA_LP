c6102832bdd74760f02bf093747aab3af55d0aba
LaiaPertusa <92031213+Laia-bay@users.noreply.github.com>
2025-12-02 16:23:53 +0100

    video info options added
    opcions de quin tipus de info et pot donar (5points, yuv...)

--- website/tabs/videos_tab.py
+++ website/tabs/videos_tab.py
@@ -14,6 +14,10 @@ def display():
     )
     st.write("")
     st.write("In this video processing & encoding page all transformations will be done on the Big Buck Bunny (BBB) video.")
-    st.image(BBB, "Big Buck Bunny movie video")
+    col1,col2 = st.columns(2)
+    info_options = ["---", "5 points of information", "Inspect mp4 tracks", "YUV histogram"]
+    col1.selectbox("Select the information of the video that you want", info_options)
+    col2.image(BBB, "Big Buck Bunny movie video")
 
-    
+    processing_options = ["---", "Resize video", "Create BBB container", "Macroblocks Motion Vectors"]
+
